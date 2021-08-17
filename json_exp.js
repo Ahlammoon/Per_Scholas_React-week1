c812b4b21ba9b5948cb28ba4218c5596b553b689
Ahlammoon <m.elzagadani@gmail.com>
2021-08-17 11:42:15 -0400

    Created pakage.json, JS object  examples.

---
+++ json_exp.js
@@ -0,0 +1,19 @@
+const myObject ={
+  a:"this is property a",
+  mySecondObjectProperty :"second object property",
+  "x" :"the x key on my object",
+  "2" : "the key that is number 2",
+  3:"key that is 3"
+
+}
+console.log("myObject", myObject);
+
+const myInfo={
+    "name" :"Malak",
+    "birthday": 0707,
+    "citiesLivedIn" :["Kentucky","NC"],
+    "isHuman" : true,
+     "planetsVisite" : null,
+     "personalInfo" :{"favoriteFood":"Piza",}
+};
+console.log("my Info", myInfo)
